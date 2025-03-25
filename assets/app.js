e47be625a84fd22ab62f5c43f3aa2aeef531423e
d9beuD <d9beuD@users.noreply.github.com>
2025-03-25 21:03:07 +0100

    Add Font Awesome

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,4 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './fontawesome-kit/css/all.min.css';

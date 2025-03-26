2a1f4656281140a2fa74b3e69510982db183c7c8
d9beuD <d9beuD@users.noreply.github.com>
2025-03-27 00:32:24 +0100

    Create empty applications page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,12 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/applications', name: 'app_home_applications')]
+    public function applications(): Response
+    {
+        return $this->render('home/applications.html.twig');
     }
 }
